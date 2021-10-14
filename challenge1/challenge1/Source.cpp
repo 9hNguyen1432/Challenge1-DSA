d14c2afa828c6362364ccd41de7ca8443266e42e
heoquaycutekhung <chinhnguyen140302@gmail.com>
2021-10-14 23:08:29 +0700

    update

--- challenge1/challenge1/Source.cpp
+++ challenge1/challenge1/Source.cpp
@@ -13,13 +13,13 @@ struct sList {
 	int MAX_LEVEL; // cập nhập liên tục mỗi khi thêm, xóa phần tử.
 	vector<int> sizeOfLevel; // kích thước tầng
 	sNode* headNode;// Node bắt đầu
-	sNode* tailNode;// Node kết thúc
+	sNode* NULtail;// Node kết thúc
 };
 sList initList() {
 	sList L;
 	L.MAX_LEVEL = 0;
 	L.headNode = NULL;
-	L.tailNode = NULL;
+	L.NULtail = NULL;
 	return L;
 }
 sNode* createSNode(int key, int level) {
@@ -38,7 +38,32 @@ int levelSNode(int key, int maxLevel) {
 	}
 	return (1+i)%maxLevel;
 }
-void insertSNode(sList* L, int key);
+void creatsList(sList& L) {
+	L.headNode->key = numeric_limits<int>::min();
+}
+
+void searchSNode(sList L, int key) {
+	vector<sNode*> path;
+	sNode* tempNode = L.headNode;
+	while (L.headNode->nextNode[L.MAX_LEVEL - 1]!= NULL) {
+
+	}
+}
+
+void insertSNode(sList &L, int key) {
+	int lv = levelSNode(key, L.MAX_LEVEL);// xác định level của node
+	sNode* n = createSNode(key, lv); // tạo node với lv tương ứng
+	if (L.headNode->nextNode[0] == NULL) {// ds rỗng, chèn đầu
+		for (int i = 0; i < lv; i++) {
+			n->nextNode[i] = L.headNode->nextNode[i]; // lần lượt các lv của n trỏ vào vị trí head đang trỏ
+			L.headNode->nextNode[i] = n; // trỏ các con trỏ ở từng lv của head về n
+		}
+	}
+	else {
+		// thêm vào bất kì vị trí:
+		
+	}
+}
 int main() {
 	/*sList L;
 	L.headNode = new sNode;
